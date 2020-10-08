05354b4813030b913a429b82470b8ef9612b1c1a
vakas-786 <vakas0959@gmail.com>
2020-10-07 21:56:11 -0400

    prize seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,12 +30,12 @@ Transaction.create(item: "Mini Golf", type_trans: "Expense", category: "Recreati
 Transaction.create(item: "Donation", type_trans: "Expense", category: "Misc", amount: 200.00, date: Faker::Date.between(from: '2020-01-01', to: '2020-09-30'), account_id: Account.all.first.id)
 
 
-Prize.create(image_url: 'https://i.imgur.com/JyHrUVb.png' , value: 300.00, user_id: User.all.first.id, name: "Zombie")
-Prize.create(image_url: 'https://i.imgur.com/j1URdpA.png' , value: 450.00, user_id: User.all.first.id,  name: "Jelly")
-Prize.create(image_url: 'https://i.imgur.com/OHythEC.png' , value: 800.00, user_id: User.all.first.id, name: "Robot")
-Prize.create(image_url: 'https://i.imgur.com/ibOpny9.png' , value: 375.00, user_id: User.all.first.id, name: "Cat")
-Prize.create(image_url: 'https://i.imgur.com/Dsfbaej.png' , value: 375.00, user_id: User.all.first.id, name: "Dog")
-Prize.create(image_url: 'https://i.imgur.com/lnGEpMY.png' , value: 625.00, user_id: User.all.first.id, name: "Plane")
-Prize.create(image_url: 'https://i.imgur.com/p2ULS7A.png' , value: 740.00, user_id: User.all.first.id, name: "Dinosaur")
+Prize.create(image_url: 'https://i.imgur.com/JyHrUVb.png' , value: 300.00, user_id: User.all.first.id, name: "Zombie", status: false)
+Prize.create(image_url: 'https://i.imgur.com/j1URdpA.png' , value: 450.00, user_id: User.all.first.id,  name: "Jelly", status: false)
+Prize.create(image_url: 'https://i.imgur.com/OHythEC.png' , value: 800.00, user_id: User.all.first.id, name: "Robot", status: false)
+Prize.create(image_url: 'https://i.imgur.com/ibOpny9.png' , value: 375.00, user_id: User.all.first.id, name: "Cat", status: false)
+Prize.create(image_url: 'https://i.imgur.com/Dsfbaej.png' , value: 375.00, user_id: User.all.first.id, name: "Dog", status: false)
+Prize.create(image_url: 'https://i.imgur.com/lnGEpMY.png' , value: 625.00, user_id: User.all.first.id, name: "Plane", status: false)
+Prize.create(image_url: 'https://i.imgur.com/p2ULS7A.png' , value: 740.00, user_id: User.all.first.id, name: "Dinosaur", status: false)
 
 
