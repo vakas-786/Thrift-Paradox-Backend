2f78a44476d89666469da9f0d8fa3db788b7a38b
vakas-786 <vakas0959@gmail.com>
2020-10-10 03:25:23 -0400

    remove byebug

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -24,7 +24,6 @@ class TransactionsController < ApplicationController
     end 
 
     def destroy 
-        byebug
         transaction = Transaction.find(params[:id])
         transaction.destroy 
         render json: transaction 
