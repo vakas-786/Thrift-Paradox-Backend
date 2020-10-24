2a0530cc2eed373269f2c793b857637937a08910
vakas-786 <vakas0959@gmail.com>
2020-10-24 19:59:30 -0400

    assign prize new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,19 @@ class User < ApplicationRecord
             account_user.update(token: user_token += 1)
         end 
     end
+
+    def assign_prizes(user)
+        prizes_array = []
+        
+        prizes_array.push(Prize.create(image_url: 'https://i.imgur.com/JyHrUVb.png' , value: 300.00, user_id: User.all.first.id, name: "Zombie", status: false))
+        prizes_array.push(Prize.create.(image_url: 'https://i.imgur.com/j1URdpA.png' , value: 450.00, user_id: User.all.first.id,  name: "Jelly", status: false))
+        prizes_array.push(Prize.create.(image_url: 'https://i.imgur.com/OHythEC.png' , value: 800.00, user_id: User.all.first.id, name: "Robot", status: false))
+        prizes_array.push(Prize.create.(image_url: 'https://i.imgur.com/ibOpny9.png' , value: 375.00, user_id: User.all.first.id, name: "Cat", status: false))
+        prizes_array.push(Prize.create.(image_url: 'https://i.imgur.com/Dsfbaej.png' , value: 375.00, user_id: User.all.first.id, name: "Dog", status: false))
+        prizes_array.push(Prize.create.(image_url: 'https://i.imgur.com/lnGEpMY.png' , value: 625.00, user_id: User.all.first.id, name: "Plane", status: false))
+        prizes_array.push(Prize.create.(image_url: 'https://i.imgur.com/p2ULS7A.png' , value: 740.00, user_id: User.all.first.id, name: "Dinosaur", status: false))
+
+        prizes_array = user.prizes
+    end 
+
 end
